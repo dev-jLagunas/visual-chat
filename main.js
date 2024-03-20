9d986c1787ec71df4d957fd9ae4db3af5e54d872
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-20 12:56:46 +0900

    feat(main.js): add css fade-in animation using css to control order of fade in

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import "./style.scss";
+
+document.querySelectorAll(".message").forEach((message) => {
+  const order = message.getAttribute("data-order");
+  message.style.animationDelay = `${order * 1}s`;
+});
